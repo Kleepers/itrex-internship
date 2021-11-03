468d4d5080fe7e98b63e5d65dffb71774895cb35
Kleepers <perezzzq@yandex.ru>
2021-11-03 21:42:54 +0300

    basic mvc with validation

---
+++ src/scripts/FormValidator.js
@@ -0,0 +1,73 @@
+export default class FormValidator {
+    constructor(validationSettings, formElement) {
+        this._validationSettings = validationSettings;
+        this._formElement = formElement;
+        this._inputList = Array.from(this._formElement.querySelectorAll(this._validationSettings.inputSelector));
+        this._buttonElement = this._formElement.querySelector(this._validationSettings.submitButtonSelector);
+    };
+
+    _showInputError(inputElement, errorMessage) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.add(this._validationSettings.inputErrorClass);
+
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(this._validationSettings.errorClass);
+    };
+
+    _hideInputError (inputElement) {
+        const errorElement = this._formElement.querySelector(`.${inputElement.id}-error`);
+        inputElement.classList.remove(this._validationSettings.inputErrorClass);
+        errorElement.classList.remove(this._validationSettings.errorClass);
+
+        errorElement.textContent = '';
+    };
+
+    _isValid (inputElement) {
+        if (!inputElement.validity.valid) {
+            this._showInputError(inputElement, inputElement.validationMessage);
+        }
+        else {
+            this._hideInputError(inputElement);
+        }
+    }
+
+    _hasInvalidInput () {
+        return this._inputList.some((inputElement) => {
+            return !inputElement.validity.valid;
+        })
+    }
+
+    _toggleButtonState () {
+        if (this._hasInvalidInput(this._inputList)) {
+            this._buttonElement.setAttribute('disabled', 'disabled');
+            this._buttonElement.classList.add(this._validationSettings.inactiveButtonClass);
+        }
+        else {
+            this._buttonElement.removeAttribute('disabled');
+            this._buttonElement.classList.remove(this._validationSettings.inactiveButtonClass);
+        }
+    }
+
+    _setEventListeners () {
+        this._toggleButtonState();
+
+        this._inputList.forEach((inputElement) => {
+            inputElement.addEventListener('input', () => {
+                this._isValid(inputElement);
+                this._toggleButtonState();
+            });
+        });
+    };
+
+    clearErrors() {
+        this._inputList.forEach((inputElement) => {
+            this._hideInputError(inputElement);
+        });
+        this._toggleButtonState();
+    };
+
+    enableValidation () {
+        console.log(this._inputList)
+        this._setEventListeners();
+    };
+}
---
+++ src/scripts/constants.js
@@ -0,0 +1,67 @@
+const signUpInputList = [
+    {
+        icon: 'icon-user',
+        type: 'text',
+        placeholder: 'First Name',
+        name: 'firstName'
+    },
+    {
+        icon: 'icon-user',
+        type: 'text',
+        placeholder: 'Last Name',
+        name: 'lastName'
+    },
+    {
+        icon: 'icon-mail',
+        type: 'email',
+        placeholder: 'Email',
+        name: 'email'
+    },
+    {
+        icon: 'icon-pass',
+        type: 'password',
+        placeholder: 'Password',
+        name: 'password'
+    },
+    {
+        icon: 'icon-confirm-pass',
+        type: 'password',
+        placeholder: 'Confirm password',
+        name: 'confirmPassword'
+    }
+]
+
+const signInInputList = [
+    {
+        icon: 'icon-mail',
+        type: 'email',
+        placeholder: 'Email',
+        name: 'email'
+    },
+    {
+        icon: 'icon-pass',
+        type: 'password',
+        placeholder: 'Password',
+        name: 'password'
+    }
+]
+
+const restorePasswordInputList = [
+    {
+        icon: 'icon-mail',
+        type: 'email',
+        placeholder: 'Email',
+        name: 'email'
+    },
+]
+
+const validationSettings = ({
+    formSelector: '.authorization',
+    inputSelector: '.input',
+    submitButtonSelector: '.authorization__submit',
+    inactiveButtonClass: 'authorization__submit_disabled',
+    inputErrorClass: 'input-invalid',
+    errorClass: 'input-error'
+})
+
+export { validationSettings, restorePasswordInputList, signInInputList, signUpInputList }
